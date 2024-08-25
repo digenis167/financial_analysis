97238551b2c2a4f3d7c9f76828dbde0f00ae36ec
Antreas Digeni <antreasdigeni@gmail.com>
2024-08-25 17:34:12 +0300

    Few change for cleaner code

--- financial_analysis.py
+++ financial_analysis.py
@@ -14,33 +14,28 @@ def get_stock_data(ticker, start_date = '2024-07-22', end_date = '2024-08-22'):
     data = yf.download(ticker, start=start_date, end=end_date)
     return data
 
+# Tickers to use for the application
 tickers_list = {'Apple': 'AAPL', 'Microsoft': 'MSFT', 'Tesla': 'TSLA', 'NVIDIA Corp': 'NVDA', 'Intel Corp': 'INTC', 'Amazon.com Inc': 'AMZN',
                 'Advanced Micro Devices, Inc.': 'AMD', 'Alphabet Inc Class C': 'GOOG', 'Marvell Technology Inc': 'MRVL', 'Arm Holdings PLC': 'ARM',
                 'Airbnb Inc': 'ABNB', 'Qualcomm Inc': 'QCOM', 'Atlassian Corp': 'TEAM', 'Texas Instruments Inc': 'TXN', 'NXP Semiconductors NV': 'NXPI'
 }
-
-
+# Set the layout of the web application to wide
 st.set_page_config(layout='wide')
-
 # WebApp Title
 st.title('Interactive Financial Stock Market Comperative Analysis Tool')
 
 # Set Default Date Range and Display date_input
 end_date = datetime.date.today()
 start_date = end_date - relativedelta(months=6)
-
 # Setup User input text boxes with defaults as AAPL and GOOGL
 st.sidebar.header('User Input Options')
 selected_stock = st.sidebar.text_input('Enter Stock Ticker', 'AAPL').upper()
 selected_stock2 = st.sidebar.text_input('Enter Stock Ticker 2', 'GOOGL').upper()
-
 # Query Stock Data
-stock_data = get_stock_data(selected_stock, start_date, end_date) #, selected_date[0], selected_date[1])
-stock_data2 = get_stock_data(selected_stock2, start_date, end_date) #, selected_date[0], selected_date[1])
-
+stock_data = get_stock_data(selected_stock, start_date, end_date)
+stock_data2 = get_stock_data(selected_stock2, start_date, end_date)
 # Create 2 columns for displaying the data
 col1, col2 = st.columns(2)
-
 # Display Selected Stock Data
 with col1:
     st.subheader(f"Displaying data for: {selected_stock}")
